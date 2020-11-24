eff2af5ff598dd46ba6817764f1d9bcfe41a166c
Luis <lm.perezpacheco@gmail.com>
2020-11-25 00:36:39 +0100

    init service gradebook and web

---
+++ modules/gradebook/gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/GradebookPortlet.java
@@ -0,0 +1,28 @@
+package com.liferay.training.gradebook.web.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.training.gradebook.web.constants.GradebookPortletKeys;
+import org.osgi.service.component.annotations.Component;
+
+import javax.portlet.Portlet;
+
+/**
+ * @author luis
+ */
+@Component(
+        immediate = true,
+        property = {
+                "com.liferay.portlet.display-category=category.training",
+                "com.liferay.portlet.instanceable=false",
+                "com.liferay.portlet.header-portlet-css=/css/main.css",
+                "javax.portlet.display-name=Gradebook",
+                "javax.portlet.init-param.template-path=/",
+                "javax.portlet.init-param.view-template=/view.jsp",
+                "javax.portlet.name=" + GradebookPortletKeys.GRADEBOOK,
+                "javax.portlet.resource-bundle=content.Language",
+                "javax.portlet.security-role-ref=power-user,user"
+        },
+        service = Portlet.class
+)
+public class GradebookPortlet extends MVCPortlet {
+}
---
+++ modules/gradebook/gradebook-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/META-INF/resources/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="gradebook.caption"/></b>
+</p>
