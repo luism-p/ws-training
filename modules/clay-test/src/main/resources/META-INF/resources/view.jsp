15d259252810f6e904efed9046f5ecad73b49e45
Luis <lm.perezpacheco@gmail.com>
2021-02-03 00:07:14 +0100

    add function theme

--- modules/clay-test/src/main/resources/META-INF/resources/view.jsp
+++ modules/clay-test/src/main/resources/META-INF/resources/view.jsp
@@ -9,6 +9,31 @@
         <aui:input name="text" type="text">
 			<aui:validator name="gmail"/>
         </aui:input>
+
+        <aui:input type="text" name="fecha" label="fecha">
+
+            <aui:validator name="dateCustom"/>
+        </aui:input>
     </aui:fieldset>
 
 </aui:form>
+
+<aui:script>
+    YUI().use(
+            'aui-datepicker',
+            function(Y) {
+                new Y.DatePicker(
+                        {
+                            trigger: '#<portlet:namespace/>fecha',
+                            mask: '%d/%m/%Y',
+                            popover: {
+                                zIndex: 9999
+                            },
+                            on: { selectionChange: function(event) {
+                            }
+                            }
+                        }
+                );
+            }
+    );
+</aui:script>
--- themes/livingstone-fjord-theme/src/js/main.js
+++ themes/livingstone-fjord-theme/src/js/main.js
@@ -36,6 +36,10 @@ AUI().use('aui-form-validator', function (A) {
 
                 return value.endsWith('@liferay.com');
             },
+            dateCustom:function  (value, fieldNode, ruleValue){
+
+                return parseStringToDate(value, 0,0,0,0) !== undefined;
+            }
         },
         true
     );
@@ -43,8 +47,39 @@ AUI().use('aui-form-validator', function (A) {
     A.mix(
         defaultFormValidator.STRINGS,
         {
-            gmail: Liferay.Language.get('please-enter-a-valid-gmail-address')
+            gmail: Liferay.Language.get('please-enter-a-valid-gmail-address'),
+            dateCustom: Liferay.Language.get('please-enter-a-valid-date')
         },
         true
     );
 });
+
+ const parseStringToDate = function (strDate, hours, minutes, seconds, miliSeconds) {
+     console.log(strDate);
+    const REGEX_SP_DATE = /[.,\/ -]/;
+    hours = !isNaN(hours) ? hours : 0;
+    minutes = !isNaN(minutes) ? minutes : 0;
+    seconds = !isNaN(seconds) ? seconds : 0;
+    miliSeconds = !isNaN(miliSeconds) ? miliSeconds : 0;
+
+    if (strDate && REGEX_SP_DATE.exec(strDate)) {
+        var tkDate = strDate.split(REGEX_SP_DATE);
+
+        if (tkDate[0].length > 2) {
+            var date = new Date(tkDate[0], tkDate[1] - 1, tkDate[2], hours, minutes, seconds, miliSeconds);
+
+            return compareDate(tkDate[2], tkDate[1], tkDate[0], date) ? date: undefined;
+       } else {
+            var date = new Date(tkDate[2], tkDate[1] - 1, tkDate[0], hours, minutes, seconds, miliSeconds);
+
+            return compareDate(tkDate[0], tkDate[1], tkDate[2], date) ? date: undefined;
+        }
+    } else {
+        return undefined;
+    }
+}
+
+
+const compareDate = function (day, month, year, date){
+     return date.getDay() === +day && (date.getMonth() +1) === +month && date.getFullYear() === +year;
+}
